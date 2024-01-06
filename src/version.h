6bbeae8369cf9a3241e9eea2f90172b53c20f6d1
ZDEEX <95095652+ZDEEX@users.noreply.github.com>
2024-01-06 16:38:30 +0700

    Update version.h

--- src/version.h
+++ src/version.h
@@ -21,13 +21,13 @@
 #define HUSH_VERSION_H
 
 // network protocol versioning
-static const int PROTOCOL_VERSION = 1987426;
+static const int PROTOCOL_VERSION = 1987440;
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
 //! In this version, 'getheaders' was introduced.
 static const int GETHEADERS_VERSION = 31800;
 //! disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = 1987420;
+static const int MIN_PEER_PROTO_VERSION = 1987430;
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
 static const int CADDR_TIME_VERSION = 31402;
